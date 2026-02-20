429c25a438acdf752de1a9ae71069a9c549cbd2d
monish <monishgowda27@gmail.com>
2026-02-20 13:43:17 +0530

    added cross join

---
+++ joins/cross_join.sql
@@ -0,0 +1,15 @@
+use joins;
+
+create table cj(
+id int primary key,
+name varchar(20));
+insert into cj values (1,"monish"),(2,"puneeth");
+select * from cj;
+
+create table cj1(
+city varchar(20));
+insert into cj1 values ("bengaluru"),("mysore");
+select * from cj1;
+
+select * from cj cross join cj1 order by id asc;
+select name,city from cj cross join cj1;
