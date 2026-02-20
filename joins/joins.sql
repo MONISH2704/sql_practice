a9feaa601720044358a8def87d800a59adad35ab
monish <monishgowda27@gmail.com>
2026-02-20 13:42:47 +0530

    added joins

---
+++ joins/joins.sql
@@ -0,0 +1,25 @@
+create database joins;
+
+use joins;
+
+create table student(
+student_id int primary key,
+name varchar(20));
+insert into student values (101,"adam"),(102,"bob"),(103,"casey");
+select * from student;
+create table course(
+student_id int primary key,
+course varchar(20));
+insert into course values (102,"english"),(105,"math"),(103,"science"),(107,"computer");
+select * from course;
+
+select * from student inner join course on student.student_id = course.student_id;
+select * from student left join course on student.student_id=course.student_id;
+select * from student right join course on student.student_id=course.student_id;
+
+select * from student left join course on student.student_id=course.student_id
+union
+select * from student right join course on student.student_id=course.student_id;
+
+select * from student left join course on student.student_id=course.student_id where course.student_id is null;
+select * from student right join course on student.student_id=course.student_id where student.student_id is null;
